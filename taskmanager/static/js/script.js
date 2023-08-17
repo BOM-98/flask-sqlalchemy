b8e6776d81357d05e0769b846da7b90231887571
BOM-98 <>
2023-08-17 18:02:47 +0000

    17 Aug Finishing commit

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, options);
 });
