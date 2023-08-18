077bf8403de3dad4fbf3f3d30845f73625651207
BOM-98 <>
2023-08-18 08:55:48 +0000

    add tasks to db

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    // sidenav initialization //
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
 
+    // datepicker initialization
     let datepicker = document.querySelectorAll('.datepicker');
-    M.Datepicker.init(datepicker, options);
+    M.Datepicker.init(datepicker, {
+        format: "dd mmm,yyyy",
+    });
+
+    // select initialization
+    var selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
